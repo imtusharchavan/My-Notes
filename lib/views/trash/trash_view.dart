a76c94924245c41518920dc0b8d05971e24ce7cc
Tushar Chavan <tusharrchavan014@gmail.com>
2023-02-18 19:23:25 +0530

    Added trash view

---
+++ lib/views/trash/trash_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/utilities/nav_bar.dart';
+
+class TrashView extends StatefulWidget {
+  const TrashView({super.key});
+
+  @override
+  State<TrashView> createState() => _TrashViewState();
+}
+
+class _TrashViewState extends State<TrashView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const NabBar(),
+      appBar: AppBar(
+        title: const Text(
+          'Trash',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+          ),
+        ),
+        iconTheme: const IconThemeData(color: Colors.black),
+        elevation: 0,
+        backgroundColor: Colors.white10,
+      ),
+    );
+  }
+}
