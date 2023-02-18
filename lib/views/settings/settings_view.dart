c4c25455f3cb103a09bc0276815023274a499e08
Tushar Chavan <tusharrchavan014@gmail.com>
2023-02-18 19:23:45 +0530

    Added settings view

---
+++ lib/views/settings/settings_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SettingsView extends StatefulWidget {
+  const SettingsView({super.key});
+
+  @override
+  State<SettingsView> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Settings',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+          ),
+        ),
+        iconTheme: const IconThemeData(color: Colors.black),
+        elevation: 0,
+        backgroundColor: Colors.white10,
+      ),
+    );
+  }
+}
