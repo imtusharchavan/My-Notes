941a40f33e573c3b26169ba042a3c06b75210ff9
Tushar Chavan <tusharrchavan014@gmail.com>
2023-02-24 00:00:45 +0530

    Handle auth bloc exceptions during login

--- lib/services/auth/auth_exceptions.dart
+++ lib/services/auth/auth_exceptions.dart
@@ -3,5 +3,5 @@ class WrongPasswordAuthException implements Exception {}
 class WeakPasswordAuthException implements Exception {}
 class EmailAlreadyInUseAuthException implements Exception {}
 class InvalidEmailAuthException implements Exception {}
-class GenericAuthException implements Exception {}
 class UserNotLoggedInAuthException implements Exception {}
+class GenericAuthException implements Exception {}
